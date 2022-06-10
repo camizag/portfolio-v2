72bb63118c255a5ea8ab16f4b05492a20c8d8015
Camila Zagal <camilazags@gmail.com>
2022-06-10 11:46:56 -0500

    inital commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.css",
+    "./assets/**/*.css",
+    "./**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
